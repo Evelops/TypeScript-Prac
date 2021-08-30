a8cb5843c5ddd3c94ea8c28cb1619a4ad68168c3
eomseungyeol <co6747@naver.com>
2021-08-30 21:20:11 +0900

    # ts test

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("hello!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "SeungYeol", age = 25, gender = "male";
+const SayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+SayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
